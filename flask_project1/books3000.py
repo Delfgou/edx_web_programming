04d22363d338d7dcd2537954d65d4a5dae635649
delfgcyr <delfgcyr@students.zhaw.ch>
2019-01-30 19:00:41 +0100

    moved success.html to templates

--- flask_project1/books3000.py
+++ flask_project1/books3000.py
@@ -4,14 +4,15 @@ from models import *
 
 app = Flask(__name__)
 #app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
-#app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-#db.init_app(app)
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://vhltvfuekxyisp:4e8e3284506f4903c26843cbeca2e8bc2ed4693c95ddd2dd8f104550c5700e27@ec2-79-125-6-250.eu-west-1.compute.amazonaws.com:5432/d4j9oravts15j7"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db.init_app(app)
 
 @app.route("/")
 def index():
     return render_template('index.html')
 
-@app.route("/register")
+@app.route("/register", methods=["POST"])
 def register():
     username = request.form.get("username")
     email = request.form.get("email")
