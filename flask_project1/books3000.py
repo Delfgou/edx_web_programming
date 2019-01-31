a1a075798bcaaf50dddbf2e5299e767d55faa333
delfgcyr <delfgcyr@students.zhaw.ch>
2019-01-31 16:55:36 +0100

    books can be search for all columns

--- flask_project1/books3000.py
+++ flask_project1/books3000.py
@@ -55,6 +55,35 @@ def sign_in():
 @app.route("/sign_out", methods=["POST"])
 def sign_out():
     return render_template('index.html', message="You are logged out")
+
+@app.route("/search", methods=["POST"])
+def search():
+    #isbn = request.form.get("isbn")
+    #title = request.form.get("title")
+    #author = request.form.get("author")
+    #year = request.form.get("year")    
+    #if isbn is None:
+        #if title is None:
+            #if author is None:
+                #results = Book.query.filter_by(year = year).all()                                
+            #else: 
+                #results = Book.query.filter_by(author = author).all()                
+        #else:
+            #results = Book.query.filter_by(title = title).all()            
+    #else:
+        #results = Book.query.filter_by(isbn = isbn).all()
+    #return render_template('search_results.html', results = results)
+    isbn = request.form.get("isbn")
+    title = request.form.get("title")
+    author = request.form.get("author")
+    year = request.form.get("year")    
+    results_isbn = Book.query.filter_by(isbn = isbn).all()   
+    results_title = Book.query.filter_by(title = title).all()        
+    results_author = Book.query.filter_by(author = author).all()        
+    results_year = Book.query.filter_by(year = year).all()        
+    
+                
+    return render_template('search_results.html', results_isbn = results_isbn, results_title = results_title, results_author = results_author, results_year = results_year)    
     
 
        
--- flask_project1/models.py
+++ flask_project1/models.py
@@ -9,4 +9,10 @@ class User(db.Model): #or db.database_name ?
     email = db.Column(db.String, nullable = False)
     password = db.Column(db.String, nullable = False)
     
-    
+class Book(db.Model):
+    __tablename__="books"
+    id = db.Column(db.Integer, primary_key = True)
+    isbn = db.Column(db.String)
+    title = db.Column(db.String)
+    author = db.Column(db.String)
+    year = db.Column(db.String)
